d3f9d3282204a63b4ffa66e2213649d1ea1ab6d4
Utsav <utsavthapa901@gmail.com>
2024-04-14 14:16:10 +0545

    Week 1 Task 1 comment

--- week1/hello.java
+++ week1/hello.java
@@ -2,6 +2,7 @@ package week1;
 
 public class hello{
     public static void main (String[] args){
+        /*1. Write a program that prints Hello World! to the console. Save the program as Hello.java. */
         System.out.println("Hello world");
 
     }
